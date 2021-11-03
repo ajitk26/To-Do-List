289bac3daa3e61296ade1cf771ec791899d71599
Ajit kumar <aky8062@gmail.com>
2021-11-03 10:29:43 +0530

    complete

--- js/logic.js
+++ js/logic.js
@@ -4,21 +4,22 @@ $("ul").on("click", "li", function(){
 });
 
 //click on X to delete the to-do
-$("span").on("click",function(event){
-	$(this).parent().fadeOut(500,function(){
+$("ul").on("click", "span",function(event){
+	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
+	
 	event.stopPropagation();
 
 });
 
-$("input[type='text'").keypress(function(){
+$("input[type='text']").keypress(function(event){
 	if(event.which === 13){
 		//inserting new To-do from input
 		let toDoText=$(this).val();
 		$(this).val("");
 
 		//create a new li and add to ul
-		$("ul").append("<li><span>X</span>" + toDoText + "</li>")
+		$("ul").append("<li><span><i class='fas fa-trash'></i></span>" + toDoText + "</li>")
 	}
 });
